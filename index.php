70adb482b02c565d02a0b5aa288ed91738e14ab0
Martin.M <martin.myrmarker@gmail.com>
2018-10-09 22:14:22 +0200

    init

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Martin.M</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+</head>
+<body>
+<div id="container">
+    <div id="content">
+        <nav>
+            <div class="logo">
+
+            </div>
+            <div class="links">
+                <a href="">Test</a>
+                <a href="">Test</a>
+                <a href="">Test</a>
+                <a href="">Test</a>
+            </div>
+            <div class="socialIcons">
+
+            </div>
+        </nav>
+        <div id="start">
+            <h1 class="start-text">Martin</h1>
+            <h1 class="start-text">Myrmarker</h1>
+            <p class="start-p">PROGRAMMING, BACKEND, FRONTEND, CSS, PHP, SQL</p>
+        </div>
+        <div id="case">
+            <h1 class="caseHeader">CASE</h1>
+            <div id="caseBoxWrapper">
+                <div class="caseBox"></div>
+                <div class="caseBox"></div>
+                <div class="caseBox"></div>
+            </div>
+        </div>
+        <div id="about">
+            <div class="portrait">
+                <div class="portraitImg"></div>
+            </div>
+            <div class="textAboutMe">
+                <p>
+                   text om mig....
+                </p>
+            </div>
+            <div class="aboutHeader">
+                <h1 class="aboutH">OM</h1>
+            </div>
+        </div>
+        <div id="contact">
+            <div class="socialIcons">
+                <i class="fab fa-linkedin"></i>
+            </div>
+            <div class="contact-info">
+                <p>Martin Myrmarker</p>
+                <p>martin@myrmarker.se</p>
+                <p>070-7945070</p>
+                <br/>
+                <p>eller</p>
+            </div>
+            <form class="emailForm" method="post">
+                <input type="text"/><br/>
+                <input type="text"/><br/>
+                <input type=""/><br/>
+                <input type="submit"/>
+            </form>
+            <h1 class="contactHeader">KONTAKT </h1>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
