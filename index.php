9a8b868fae9aa277f2628f669d362c25fd104a4e
Martin.M <martin.myrmarker@gmail.com>
2018-11-06 15:30:18 +0100

    changing the layout

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Martin.M</title>
+    <title>Martin Myrmarker | Webbutvecklare</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet">
@@ -11,77 +11,28 @@
 <div id="container">
     <div id="content">
 
-            <div class="navbar" id="nav">
-                <div class="logo"></div>
-                <div class="linkWrapper">
-                    <a href="#content">Hem</a>
-                    <a href="#case">Case</a>
-                    <a href="#about">Om</a>
-                    <a href="#contact">Kontakt</a>
-                </div>
-                <div class="socialIconsStart">
-                    <i class="fab fa-linkedin fa-2x"></i>
-                    <i class="fab fa-github fa-2x"></i>
-                </div>
-            </div>
-
-        <div id="start">
-            <h1 class="start-text">Martin</h1>
-            <h1 class="start-text">Myrmarker</h1>
-            <p class="start-p">PROGRAMMING - BACKEND - FRONTEND</p>
+        <div class="socialIcons">
+            <i class="fab fa-linkedin fa-3x"></i>
+            <i class="fab fa-github fa-3x"></i>
         </div>
-        <div id="case">
-            <h1 class="caseHeader">CASE</h1>
-            <div id="caseBoxWrapper">
-                <div class="caseBox"><h2>COMING SOON</h2></div>
-                <div class="caseBox"><h2>COMING SOON</h2></div>
-                <div class="caseBox"><h2>COMING SOON</h2></div>
+        <div id="flex-wrapper">
+            <div class="popup-aboutme pop">
+                <i class="fas fa-times fa-1x"></i>
             </div>
+            <div id="start">
+                <h1 class="start-text">Martin</h1>
+                <h1 class="start-text">Myrmarker</h1>
+                <p class="start-p">PROGRAMMING - BACKEND - FRONTEND</p>
+            </div>
+            <div class="popup-contact pop"></div>
         </div>
-        <div id="about">
-            <div class="portrait">
-                <div class="portraitImg"></div>
-            </div>
-            <div class="textAboutMe">
-                <p>
-                   text om mig....
-                </p>
-            </div>
-            <div class="aboutHeader">
-                <h1 class="aboutH">OM</h1>
-            </div>
+        <div id="buttons">
+            <button class="popup-button">Om mig</button>
+            <button class="popup-button">Projekt</button>
+            <button class="popup-button">Kontakt</button>
+
         </div>
-        <div id="contact">
 
-            <div class="contactWrapper">
-                <h1 class="contactHeader">KONTAKT</h1>
-                <div class="formWrapper">
-                    <div class="socialIcons">
-                        <i class="fab fa-linkedin fa-2x"></i>
-                        <i class="fab fa-github fa-2x"></i>
-                    </div>
-                    <div class="contact-info">
-                        <p>Martin Myrmarker</p>
-                        <p>martin@myrmarker.se</p>
-                        <p>070-7945070</p>
-
-                        <p>eller:</p><br/>
-                    </div>
-                    <form class="emailForm" method="post">
-                        <label>Namn</label><br/>
-                        <input name="name" class="contactForm" type="text"/><br/>
-                        <label>Email</label><br/>
-                        <input name="email" class="contactForm" type="email"/><br/>
-                        <label>Meddelande</label><br/>
-                        <textarea name="message" class="contactForm"></textarea><br/>
-                        <button class="submitButton" type="submit" >Skicka</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<script src="script.js"></script>
+    <script src="script.js"></script>
 </body>
 </html>
