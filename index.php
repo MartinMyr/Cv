ac8899abf53ee264e5c2dd8deb0a0833d8291109
Martin.M <martin.myrmarker@gmail.com>
2018-11-08 18:44:01 +0100

    responsive changes

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
@@ -15,21 +16,25 @@
             <i class="fab fa-linkedin fa-3x"></i>
             <i class="fab fa-github fa-3x"></i>
         </div>
-        <div id="flex-wrapper">
-            <div class="popup-aboutme pop">
-                <i class="fas fa-times fa-1x"></i>
-            </div>
+
+
+
             <div id="start">
                 <h1 class="start-text">Martin</h1>
                 <h1 class="start-text">Myrmarker</h1>
                 <p class="start-p">PROGRAMMING - BACKEND - FRONTEND</p>
             </div>
-            <div class="popup-contact pop"></div>
+
+        <div class="mobile-about">
+            <i class="fas fa-times fa-1x"></i>
+        </div>
+        <div class="mobile-contact">
+            <i class="fas fa-times fa-1x"></i>
         </div>
         <div id="buttons">
-            <button class="popup-button">Om mig</button>
+            <a href=""><button class="popup-button about-button">Om mig</button></a>
             <button class="popup-button">Projekt</button>
-            <button class="popup-button">Kontakt</button>
+            <button class="popup-button contact-button">Kontakt</button>
 
         </div>
 
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $( document ).ready(function() {
+    $(".fa-times").hide();
+    $(".text-aboutme>p").hide();
 
     $( ".fa-linkedin" ).click(function() {
         window.open(' https://www.linkedin.com/in/martin-myrmarker-26b072150', '_blank');
@@ -8,8 +10,39 @@ $( document ).ready(function() {
     });
 
 
-    
+  /*  $( ".about-button" ).click(function() {
+        var textAboutMe = "<p>\n" +
+            "Hej!<br/><br/>\n" +
+            "Martin heter jag, 25år och bosatt i Göteborg.<br/>\n" +
+            "Pluggar just nu webbutveckling inom e-handel på Medieinstitutet och extrajobbar som webbutvecklare på Direktonline.<br/>\n" +
+            "Brinner för teknik och utveckling\n" +
+            "</p>"
 
+        $(".popup-aboutme").css("background-color", "rgba(255, 255, 255, 0.31)");
+        //$(".popup-aboutme").css("border", "1px white solid");
+        $(".popup-aboutme>i").show();
+        $(".popup-aboutme>.text").html(textAboutMe);
+
+    });
+
+    $( ".contact-button" ).click(function() {
+        var textContact = "<p>Martin Myrmarker<br/><br/>"+
+            "martin@myrmarker.se<br/><br/>"+
+            "070 7945070"
+        $(".popup-contact").css("background-color", "rgba(255, 255, 255, 0.31)");
+        //$(".popup-contact").css("border", "1px white solid");
+        $(".popup-contact>i").show();
+        $(".popup-contact>.text").html(textContact);
+
+
+    });
+
+    $(".fa-times").click(function(){
+        $(this).parents('.pop').css("background-color", "rgba(255, 255, 255, 0.00)");
+        $(this).hide();
+        $(this).next().children().remove();
+
+    });*/
 
 
 
